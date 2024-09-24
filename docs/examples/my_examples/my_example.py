f36548375e9d84b469846277df449b193443f0a3
Martinho Rial <martinho1205@gmail.com>
2024-09-24 14:23:07 +0200

    Authomatized generation of particle emission times

--- docs/examples/my_examples/my_example.py
+++ docs/examples/my_examples/my_example.py
@@ -4,36 +4,67 @@
 import pdb
 import os
 from datetime import datetime, timedelta
-import xarray as xr
+import pandas as pd
 import numpy as np
 
+
 import plasticparcels as pp
-from parcels import FieldSet,AdvectionRK4
+from parcels import FieldSet
 
 output_file = '/mnt/c/Users/martino.rial/Escritorio/resultados_parcels/my_example.zarr'
 settings_file = '/mnt/c/Users/martino.rial/Escritorio/git/plasticparcels/docs/examples/my_examples/my_example_settings.json'
 
 
 def create_fieldset_from_netcdf(settings):
+    """Constructor method to create a Parcels.Fieldset with all fields necessary for a plasticparcels simulation
+
+    Parameters
+    ----------
+    settings :
+        A dictionary of model settings used to create the fieldset
+
+    Returns
+    -------
+    fieldset
+        A parcels.FieldSet object
+    """
+
     nc_files_path = settings['ocean']['directory']
     ocean_mesh = settings['ocean']['ocean_mesh']
-    nc_files = [os.path.join(nc_files_path,arquivo) for arquivo in os.listdir(nc_files_path) if arquivo.endswith('.nc4')]
+    bathymetry_mesh = settings['ocean']['bathymetry_mesh']
+    nc_files = [os.path.join(nc_files_path, arquivo) for arquivo in os.listdir(nc_files_path) if arquivo.endswith('.nc4')]
     nc_files = sorted(nc_files)
+
     variables = {'U': 'u',
-                 'V': 'v'}
-                 #'absolute_salinity': 'salt',
-                 #'conservative_temperature': 'temp'}
-    dimensions = {"U": {"lon": "lon",
-                        "lat": "lat",
-                        "time": "time"},
-                  "V": {"lon": "lon",
-                        "lat": "lat",
-                        "time": "time"}}
-    filenames = {'U': {'lon': ocean_mesh, 'lat': ocean_mesh, 'data': nc_files},
-                 'V': {'lon': ocean_mesh, 'lat': ocean_mesh, 'data': nc_files}}
-    # Add grid type
-    # This would allow to use a certain grid type --> CHECK IF A-GRID OR C-GRID
-    #fieldset = FieldSet.from_netcdf(nc_files, variables, dimensions, allow_time_extrapolation=True)
+                 'V': 'v',
+                 'W': 'w',
+                 'absolute_salinity': 'salt',
+                 'conservative_temperature': 'temp',
+                 'bathymetry': 'bathymetry'}
+
+    dimensions = {'U': {'lon': 'lon', 'lat': 'lat', 'depth': 'depth', 'time': 'time'},
+                  'V': {'lon': 'lon', 'lat': 'lat', 'depth': 'depth', 'time': 'time'},
+                  'W': {'lon': 'lon', 'lat': 'lat', 'depth': 'depth', 'time': 'time'},
+                  'absolute_salinity': {'lon': 'lon', 'lat': 'lat', 'depth': 'depth', 'time': 'time'},
+                  'conservative_temperature': {'lon': 'lon', 'lat': 'lat', 'depth': 'depth', 'time': 'time'},
+                  'bathymetry': {'lon': 'lon', 'lat': 'lat', 'depth': 'depth'}}
+
+    filenames = {'U': {'lon': ocean_mesh, 'lat': ocean_mesh, 'depth': ocean_mesh, 'data': nc_files},
+                 'V': {'lon': ocean_mesh, 'lat': ocean_mesh, 'depth': ocean_mesh, 'data': nc_files},
+                 'W': {'lon': ocean_mesh, 'lat': ocean_mesh, 'depth': ocean_mesh, 'data': nc_files},
+                 'absolute_salinity': {'lon': ocean_mesh, 'lat': ocean_mesh, 'depth': ocean_mesh, 'data': nc_files},
+                 'conservative_temperature': {'lon': ocean_mesh, 'lat': ocean_mesh, 'depth': ocean_mesh, 'data': nc_files},
+                 'bathymetry': {'lon': ocean_mesh, 'lat': ocean_mesh, 'data': bathymetry_mesh}}
+
+    if not settings['use_3D']:
+        variables.pop('W', None)
+        dimensions.pop('W', None)
+        filenames.pop('W', None)
+        for key, value in dimensions.items():
+            value.pop('depth', None)
+        for key, value in filenames.items():
+            value.pop('depth', None)
+
     fieldset = FieldSet.from_netcdf(filenames, variables, dimensions, allow_time_extrapolation=True)
 
     fieldset.add_constant('use_mixing', settings['use_mixing'])
@@ -42,49 +73,87 @@ def create_fieldset_from_netcdf(settings):
     fieldset.add_constant('use_wind', settings['use_wind'])
     fieldset.add_constant('G', 9.81)  # Gravitational constant [m s-2]
     fieldset.add_constant('use_3D', settings['use_3D'])
+    fieldset.add_constant('z_start', 0.5)
     return fieldset
 
-def caudal_reader(settings):
-    ulla = settings['sources']['river']
-    total_days = settings['simulation']['runtime']
-    total_days = total_days.days
-    with open(ulla, 'r') as ulla_file:
-        ulla_dat = ulla_file.read()
-    lines = ulla_dat.split('\n')
-    times = []   # tempos
-    ratios = []   # ratios
-    ler_datos = False
-    for line in lines:
-        if "<BeginTimeSerie>" in line:
-            ler_datos = True
-        elif "<EndTimeSerie>" in line:
-            ler_datos = False
-        elif ler_datos:
-            elements = line.split()
-            if len(elements) == 2:
-                times.append(int(elements[0]))
-                ratios.append(float(elements[1]))
-    arr_ratios = np.array(ratios)
-    arr_ratios = arr_ratios[:total_days]
 
-    intervals = 1/arr_ratios
-    npart = int(round(sum(arr_ratios*3600*24)))
+def select_period(start_date, end_date, data):
+    """ Select data within a certain period from CSV with dates column """
+    data = data[(data['Data'] >= start_date)  # "Data" means "date" in Galician
+                & (data['Data'] <= end_date)].copy()
+    return data
+
+
+def generate_emission_times(settings):
+    """ Method to read a CSV file containing daily river flowrate
+    Distrubute particles along the study period according to this flowrate
+
+    Parameters
+    ----------
+    settings:
+        A dictionary of model settings containing simluation parameters
+
+    Returns
+    -------
+    emission_times:
+        A list containing each particle's emission time (seconds since start)
+    """
+
+    flowrate_file = settings['sources']['river']
+    npart = settings['sources']['npart']
+    runtime = settings['simulation']['runtime']
+    start = settings['simulation']['startdate']
+    end = start + runtime
+    start_date = start.strftime('%Y-%m-%d-%H:%M:%S')
+    end_date = end.strftime('%Y-%m-%d-%H:%M:%S')
+
+    flowrate = pd.read_csv(flowrate_file)
+    period = select_period(start_date, end_date, flowrate)
+    period.loc[:, 'Valor'] = period['Valor'].replace(to_replace=-9999.0, value=np.nan)
+    period.loc[:, 'Valor'] = np.abs(period['Valor'])
+    period.loc[:, 'Valor'] = period['Valor'].interpolate()
+    caudal_diario = period['Valor'].tolist()
+    caudal_diario = np.array(caudal_diario)
+    total = np.sum(caudal_diario)
+
+    relative_caudal = caudal_diario / total
+
+    daily_rates = npart * relative_caudal
+    rates = daily_rates / (3600 * 24)
+    intervals = 1 / rates
 
     emission_times = np.zeros(npart)
-    count=0; j=0; day=1
+    count = 0
+    j = 0
+    day = 1
     for i in range(npart):
         emission_times[i] = count
         count = count + intervals[j]
-        if int(count) > day*24*3600:
-            j = j+1
-            day = day+1
+        if int(count) > day * 24 * 3600:
+            j = j + 1
+            day = day + 1
             if j == len(intervals):
                 break
 
-    return emission_times, npart
+    return emission_times
+
 
 def particle_dictionary(settings):
-    emission_times, npart = caudal_reader(settings)
+    """ Creates a dictionary containing the necessary data for each particle
+    in order to create the particle set
+
+    Parameters
+    ----------
+    settings:
+        A dictionary of model settings used to create the fieldset
+
+    Returns
+    -------
+    pdict:
+        dictionary containing lat, lon and emission time for each particle
+    """
+    npart = settings['sources']['npart']
+    emission_times = generate_emission_times(settings)
     emission_times = emission_times.tolist()
     lats = npart*[settings['sources']['latitude']]
     lons = npart*[settings['sources']['longitude']]
@@ -93,12 +162,13 @@ def particle_dictionary(settings):
              "lons": lons}
     return pdict
 
+
 # Load the model settings
 settings = pp.utils.load_settings(settings_file)
 
 # Create the simulation settings
 settings['simulation'] = {
-    'startdate': datetime.strptime('2024-09-01-00:00:00', '%Y-%m-%d-%H:%M:%S'), # Start date of simulation
+    'startdate': datetime.strptime('2024-09-01-00:00:00', '%Y-%m-%d-%H:%M:%S'),  # Start date of simulation
     'runtime': timedelta(days=2),        # Runtime of simulation
     'outputdt': timedelta(hours=1),      # Timestep of output
     'dt': timedelta(minutes=30),          # Timestep of advection
@@ -106,28 +176,25 @@ settings['simulation'] = {
 
 # Overwrite some settings
 settings['use_3D'] = False
+settings['use_wind'] = False
 settings['use_biofouling'] = False
 settings['use_stokes'] = False
-settings['use_wind'] = False
 
 # Plastic type settings
 settings['plastictype'] = {
-    'wind_coefficient' : 0.01,  # Percentage of wind to apply to particles
-    'plastic_diameter' : 0.001, # Plastic particle diameter (m)
-    'plastic_density' : 1030.,  # Plastic particle density (kg/m^3)
-}
+    'wind_coefficient': 0.01,  # Percentage of wind to apply to particles
+    'plastic_diameter': 0.001,  # Plastic particle diameter (m)
+    'plastic_density': 1030.}  # Plastic particle density (kg/m^3)
 
 # Create the fieldset
 fieldset = create_fieldset_from_netcdf(settings)
 
 # Create the particleset
-emission_times, total_particles = caudal_reader(settings)  # Interval between particles for each day (in seconds)
 release_locs = particle_dictionary(settings)
 pset = pp.constructors.create_particleset(fieldset, settings, release_locs)
 
 # Create the applicable kernels to the plastic particles
-#kernels = pp.constructors.create_kernel(fieldset)
-kernels = [AdvectionRK4]
+kernels = pp.constructors.create_kernel(fieldset)
 
 runtime = settings['simulation']['runtime']
 dt = settings['simulation']['dt']
@@ -137,6 +204,4 @@ outputdt = settings['simulation']['outputdt']
 pfile = pp.ParticleFile(output_file, pset, settings=settings, outputdt=outputdt)
 
 pdb.set_trace()
-# Execute the simulation
 pset.execute(kernels, runtime=runtime, dt=dt, output_file=pfile)
-
